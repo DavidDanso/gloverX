280f482a8ab7bd506f60cea268ef497f85b36cad
DavidDanso <desiingerboy@icloud.com>
2025-02-19 11:17:30 +0000

    feat: add sitemap support and update URL patterns for consistency

--- core_a/urls.py
+++ core_a/urls.py
@@ -40,11 +40,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homePage, name='home'),
     path('robots.txt', robots_txt),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
-    path('about', aboutPage, name='about'),
-    path('services', servicePage, name='services'),
-    path('contact', contactPage, name='contact'),
-    path('tracking', trackingPage, name='tracking'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
+    path('about/', aboutPage, name='about'),
+    path('services/', servicePage, name='services'),
+    path('contact/', contactPage, name='contact'),
+    path('tracking/', trackingPage, name='tracking'),
 ]
 
 
